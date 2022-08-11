b84078df891260ab3479673a8145444820aee37f
FOH300102 <90313452+FOH300102@users.noreply.github.com>
2022-08-12 06:11:20 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -8,5 +8,5 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(command("start") & filters.private & ~filters.group & ~filters.edited)
 async def start_(client: Client, message: Message):
-    await message.reply_sticker("CAACAgUAAxkBAAEENxZiNtPdibVkMsjLZrUG9NK4hotHQgAC2wEAAoM12VSdN9ujxVtnUyME")
+    await message.reply_sticker("CAACAgUAAx0CVBD5pAACN0Ji9YV4JsPrEb02eGZ7GXQCBh0T8wAC8gIAAlWf0FQPsJVQEAW-Sx4E")
  
